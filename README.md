c6a12370ccd403b89ec10a65c9bc02aa9ee66a1c
Gabriel Gonçalves <51233554+gabrielgons@users.noreply.github.com>
2021-12-03 15:21:52 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# simple-electron-app
-Aplicação simples em Electron com atualização automática.
+# Electron com AutoUpdater na S3 
+Aplicação Electron simples para testar o AutoUpdater 
+
+#### Credenciais da AWS
+Deve ser criado na AWS um usuário com o tipo de credenciais "Access key - Programmatic access"
+
+Após criar o usuário e conceder acesso à S3 e ao bucket, as credenciais devem ser adicionadas como variáveis de ambiente na máquina de build da aplicação.
+```AWS_ACCESS_KEY_ID```
+```AWS_SECRET_ACCESS_KEY```
+
+
+#### Build
+Para Buildar:
+```npm run build```
+#### Publicação para a S3
+Para publicar na S3:
+```npm run publish```
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Notification  } = require('electron')
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
-
-  app.whenReady().then(() => {
-    createWindow()
-    checkForUpdates({ silent: true });
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: `UpDocs Versão: ${app.getVersion()}`
   })
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  autoUpdater.checkForUpdatesAndNotify()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
+
+autoUpdater.on('update-available', () => {
+  notificar('Atualização disponível', 'Aguarde o download...');
+  log.info('Atualização disponível');
+});
+
+autoUpdater.on('error', (error) => {  
+  notificar('Erro detectado', `${error}`);
+  log.error(error);
+});
+
+autoUpdater.on('update-downloaded', () => {
+  log.info('Atualização baixada');
+
+  setTimeout(() => {
+    autoUpdater.quitAndInstall();
+  }, 6000);
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});
+
+function notificar(title, body) {
+  new Notification({ title, body }).show()
+}
